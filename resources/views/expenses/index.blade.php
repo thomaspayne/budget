885b34a288ca69ade1c572568fde1e32d5af51a0
Thomas Payne <tom@darfk.net>
2017-02-26 20:53:13 +1100

    ran laravel app new, created expense model/controller, created expense index view

---
+++ resources/views/expenses/index.blade.php
@@ -0,0 +1,7 @@
+<h1>Expenses</h1>
+
+<ul>
+  @foreach $expenses as $expense
+  <li>$expense</li>
+  @endforeach
+</ul>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+
+<head>
+  <title>Budget Manager</title>
+</head>
+
+<body>
+
+<h1>Budget Manager</h1>
+
+</body>
+</html>
